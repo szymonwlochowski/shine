08ca85e96b238dcde043f768c7531b0c6e70e995
szymonwlochowski <szymonwlochowski@gmail.com>
2016-12-10 19:42:40 +0000

    Improve search query by using postgres magic.

--- db/structure.sql
+++ db/structure.sql
@@ -182,6 +182,27 @@ CREATE UNIQUE INDEX index_customers_on_email ON customers USING btree (email);
 
 
 --
+-- Name: index_customers_on_lower_email; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX index_customers_on_lower_email ON customers USING btree (lower((email)::text));
+
+
+--
+-- Name: index_customers_on_lower_first_name_varchar_pattern_ops; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX index_customers_on_lower_first_name_varchar_pattern_ops ON customers USING btree (lower((first_name)::text) varchar_pattern_ops);
+
+
+--
+-- Name: index_customers_on_lower_last_name_varchar_pattern_ops; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX index_customers_on_lower_last_name_varchar_pattern_ops ON customers USING btree (lower((last_name)::text) varchar_pattern_ops);
+
+
+--
 -- Name: index_customers_on_username; Type: INDEX; Schema: public; Owner: -
 --
 
@@ -208,6 +229,6 @@ CREATE UNIQUE INDEX index_users_on_reset_password_token ON users USING btree (re
 
 SET search_path TO "$user", public;
 
-INSERT INTO schema_migrations (version) VALUES ('20161204202443'), ('20161205230034'), ('20161210133419');
+INSERT INTO schema_migrations (version) VALUES ('20161204202443'), ('20161205230034'), ('20161210133419'), ('20161210193910');
 
 
